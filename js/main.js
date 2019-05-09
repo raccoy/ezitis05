42218cd05054a2b40555b6d6ee128b26b05b27b4
raccoy <50260116+raccoy@users.noreply.github.com>
2019-05-09 10:54:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,18 +65,3 @@ $('.js-anchor-link-menu').click(function(e){
 
 });
 // scrolll one
-$(".imgback").paroller({
-  factor: 0.2,            // multiplier for scrolling speed and offset
-  factorXs: 0.1,           // multiplier for scrolling speed and offset
-  type: 'foreground',     // background, foreground
-  direction: 'horizontal', // vertical, horizontal
-  transition: 'transform all 0.2s ease-out',//transition: 'transform .5s ease-in-out' // CSS transition
-});
-
-$(".paroller").paroller({
-  factor: -0.2,            // multiplier for scrolling speed and offset
-  factorXs: 0.1,           // multiplier for scrolling speed and offset
-  type: 'foreground',     // background, foreground
-  direction: 'horizontal', // vertical, horizontal
-  transition: 'transform all 0.2s ease-out',//transition: 'transform .5s ease-in-out' // CSS transition
-});
